35fa05c8344a41c84851487244d7644bdef11671
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-05-30 16:08:05 +1000

    Clean slate. Get product, return product.

--- ext.advanced_modifiers.php
+++ ext.advanced_modifiers.php
@@ -1,25 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Advanced Modifiers for Exp:resso's Store
- * Copyright (c) 2012 Jeremy Worboys <jeremy@complexcompulsions.com>
+ * Advanced Modifiers Extension class
+ *
+ * @package    advanced_modifiers
+ * @author     Jeremy Worboys <jeremy@complexcompulsions.com>
+ * @link       http://complexcompulsions.com
+ * @copyright  Copyright (c) 2012, Jeremy Worboys
  */
-
-Class Advanced_modifiers_ext {
+class Advanced_modifiers_ext
+{
 
     public $name            = 'Advanced Modifiers';
     public $version         = '1.0';
     public $description     = 'Extends the way you can define price modifiers in Exp:resso\'s Store Module.';
     public $settings_exist  = 'n';
-    public $docs_url        = ''; // 'http://expressionengine.com/user_guide/';
+    public $docs_url        = '';
 
-    protected $settings        = array();
+    protected $settings     = array();
 
 
     /**
      * Constructor
      *
-     * @param  mixed   Settings array or empty string if none exist.
+     * @param  mixed     Settings array or empty string if none exist.
      */
     public function __construct($settings='')
     {
@@ -34,8 +38,8 @@ Class Advanced_modifiers_ext {
      *
      * This function enters the extension into the exp_extensions table
      *
-     * @see http://codeigniter.com/user_guide/database/index.html for more
-     * information on the db class.
+     * @see    http://codeigniter.com/user_guide/database/index.html for more
+     *         information on the db class.
      *
      * @return void
      */
@@ -45,8 +49,8 @@ Class Advanced_modifiers_ext {
 
         $data = array(
             'class'     => __CLASS__,
-            'method'    => 'apply_modifier_price',
-            'hook'      => 'store_cart_item_update_end',
+            'method'    => 'store_process_product_tax',
+            'hook'      => 'store_process_product_tax',
             'settings'  => serialize($this->settings),
             'priority'  => 10,
             'version'   => $this->version,
@@ -68,6 +72,22 @@ Class Advanced_modifiers_ext {
     {
         $this->EE->db->where('class', __CLASS__);
         $this->EE->db->delete('extensions');
+    }
+
+
+    /**
+     * Store Process Product Tax
+     *
+     * This method is run after the product price values are generated. This is
+     * where we inject our code before the product is displayed to the user.
+     *
+     * @param  array     The product that we are modifying.
+     *
+     * @return array     The modified product.
+     */
+    public function store_process_product_tax($product)
+    {
+        return $product;
     }
 }
 // END CLASS
