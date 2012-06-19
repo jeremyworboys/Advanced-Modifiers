980c1bce2ece62d0d943bda7f3478e66cc018fea
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-06-19 23:56:57 +1000

    Added new table display for advanced modifier input.

--- publishing/expressionengine/third_party/advanced_modifiers/views/field.php
+++ publishing/expressionengine/third_party/advanced_modifiers/views/field.php
@@ -2,37 +2,58 @@
     <p style="margin-bottom:0.5em;">You need to enter the modifiers in the <strong>Product Details</strong> area and submit before they will become available here.</p>
 
     <?php if (isset($product) && !empty($product['modifiers'])): ?>
-    <table class="store_ft">
+    <table class="store_ft adv_mod">
         <thead>
             <tr>
-                <th style="width: 20%">Modifier Name</th>
-                <th style="width: 20%">Modifier Option</th>
-                <th style="width: 60%">Advanced Price Modifier</th>
+            <?php foreach ($product['modifiers'] as $mod): ?>
+                <?php if ($mod['mod_type'] === 'text') { continue; } ?>
+                <th><?= $mod['mod_name'] ?></th>
+            <?php endforeach ?>
+                <th>Value</th>
             </tr>
         </thead>
         <tbody>
-        <?php foreach ($product['modifiers'] as $mod): ?>
-            <?php if ($mod['mod_type'] === 'text') { continue; } ?>
-            <tr>
-                <td rowspan="<?= count($mod['options']) ?>"><?= $mod['mod_name'] ?></td>
-                <?php $first_line = true; ?>
-                <?php foreach ($mod['options'] as $opt_id => $opt): ?>
-                <?= $first_line ? '' : '<tr>' ?>
-                    <td style="border-left: 1px solid #d1d5de;"><?= $opt['opt_name'] ?></td>
-                    <td class="store_ft_text">
-                        <?= form_textarea(
-                            array(
-                                'name'        => "advanced_modifiers_field[{$opt_id}]",
-                                'value'       => (isset($advanced_modifiers[$opt_id])) ? $advanced_modifiers[$opt_id] : '',
-                                'rows'        => '6',
-                                'placeholder' => 'Use Default'
-                            )) ?>
-                    </td>
-                </tr>
-                <?php $first_line = false; ?>
-                <?php endforeach ?>
-            </tr>
-        <?php endforeach ?>
+            <?= display_rows($product['modifiers']) ?>
         </tbody>
     </table>
     <?php endif ?>
+
+<?php
+
+function display_rows($modifiers, $p=array())
+{
+    $out = '';
+    $mod = array_shift($modifiers);
+    $rowcount = row_count($modifiers);
+    foreach ($mod['options'] as $opt_id => $opt) {
+        $p[] = $opt_id;
+        $id = implode('-', $p);
+        $out .= '<tr>';
+        $out .=     '<td rowspan="'.$rowcount.'" class="adv_mod_bl">';
+        $out .=         $opt['opt_name'];
+        $out .=     '</td>';
+    if ($rowcount > 1) {
+        $out .=         display_rows($modifiers, $p);
+    } else {
+        $out .=     '<td class="store_ft_text">';
+        $out .=         form_input(array(
+                            'name' => "advanced_modifiers[$id]",
+                            'value' => (isset($advanced_modifiers[$id])) ? $advanced_modifiers[$id] : '0.00'
+                        ));
+        $out .=     '</td>';
+    }
+        $out .= '</tr>';
+        array_pop($p);
+    }
+    return $out;
+}
+
+
+function row_count($modifiers)
+{
+    $count = 1;
+    foreach ($modifiers as $mod) {
+        $count *= count($mod['options']);
+    }
+    return $count + floor($count / 2); // Don't ask, seriously
+}
